4f4d19abd5cf8fa510e3067262d42f9c4031325e
Alexcolman Tochukwu Apunike <alexcolman.apunike@gmail.com>
2022-05-22 02:22:51 -0300

    Created Conteudo, Bootcamp and Dev classes. Final Commit (1st Working Version)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import me.dio.desafio.dominio.Curso;
-import me.dio.desafio.dominio.Mentoria;
+import me.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,8 +19,65 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devCamila.getNome() + ":"  + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + devCamila.getNome() + ":"  + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devCamila.getNome() + ":"  + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devAlexcolman = new Dev();
+        devAlexcolman.setNome("Alexcolman");
+        devAlexcolman.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devAlexcolman.getNome() + ":"  + devAlexcolman.getConteudosInscritos());
+        devAlexcolman.progredir();
+        devAlexcolman.progredir();
+        devAlexcolman.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + devAlexcolman.getNome() + ":"  + devAlexcolman.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devAlexcolman.getNome() + ":"  + devAlexcolman.getConteudosConcluidos());
+        System.out.println("XP:" + devAlexcolman.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devWaleska = new Dev();
+        devWaleska.setNome("Waleska");
+        devWaleska.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devWaleska.getNome() + ":"  + devWaleska.getConteudosInscritos());
+        devWaleska.progredir();
+        devWaleska.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + devWaleska.getNome() + ":"  + devWaleska.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devWaleska.getNome() + ":"  + devWaleska.getConteudosConcluidos());
+        System.out.println("XP:" + devWaleska.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devSteve = new Dev();
+        devSteve.setNome("Steve");
+        devSteve.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos " + devSteve.getNome() + ":"  + devSteve.getConteudosInscritos());
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos " + devSteve.getNome() + ":"  + devSteve.getConteudosInscritos());
+        System.out.println("Conteudos Concluídos " + devSteve.getNome() + ":"  + devSteve.getConteudosConcluidos());
+        System.out.println("XP:" + devSteve.calcularTotalXp());
+
+
     }
 }
